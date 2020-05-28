2bc28ff33efebbdcc68f731810dbed964e2dcbd1
Jairajmehra <jairaj_mehra@yahoo.com>
2020-05-28 14:57:08 +1000

    fixes

--- ServerClass.py
+++ ServerClass.py
@@ -28,20 +28,20 @@ class Server(threading.Thread):
 
     def FetchData(self):
         frameCounter = 0
-        print(f'payload size is {self.payload_size}')
+
         while(self.Stop == False):
             frameCounter +=1
             while len(self.data) < self.payload_size:
                 self.data += self.conn.recv(16000)
             packed_msg_size = self.data[:self.payload_size]
-            print(f'packed msg size is {packed_msg_size}')
+
             self.data = self.data[self.payload_size:]
             msg_size = struct.unpack(">L", packed_msg_size)[0]
-            print(f'msg size is {msg_size}')
+
             while len(self.data) < msg_size:
                 self.data += self.conn.recv(16000)
             frame_data = self.data[:msg_size]
-            #print(f'frame')
+
             self.data = self.data[msg_size:]
             frame= pickle.loads(frame_data, fix_imports=True, encoding="bytes")
             frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
